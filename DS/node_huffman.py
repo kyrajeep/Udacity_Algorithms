f4f90132a44072f7b8a9f6ff1acf3845bedd8b1e
kyrajeep <jee_won_park@brown.edu>
2020-07-11 19:06:46 -0400

    feat: Create a node class to be imported in the Huffman Tree file

---
+++ DS/node_huffman.py
@@ -0,0 +1,17 @@
+'''
+ huffman tree:
+1. nodes as weights. build a min heap. I am so excited.
+2. take out (?) two smallest nodes
+3. combine them to make an internal node.
+'''
+class node_huffman:
+    def __init__(weight):
+        
+        self.weight = weight
+        self.left = None
+        self.right = None
+    def dummy():
+        print("hello, test")
+# I want a tree! A min heap. 
+# 
+
