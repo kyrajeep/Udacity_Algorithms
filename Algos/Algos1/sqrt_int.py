22da76c401d636bb9b7e4a6ef7f3e3a8510a1196
kyrajeep <jee_won_park@brown.edu>
2020-07-27 15:34:18 -0400

    feat: Upload worksheets from Udacity Nanodegree

---
+++ Algos/Algos1/sqrt_int.py
@@ -0,0 +1,20 @@
+#Udacity - Data Structure and Algorithms project
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    # Thought process: I think this is a binary search problem. 
+    # You can start with numbers that are smaller, maybe even numbers smaller than half 
+    # the integer as your search space.
+    pass
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
