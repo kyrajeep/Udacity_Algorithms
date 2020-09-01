58d9483118f2cefa3d9efe057b22a433b1d51ee4
kyrajeep <jee_won_park@brown.edu>
2020-09-01 13:23:07 -0400

    An attempt to make the tree class

--- DS/P3_huffmancoding.py
+++ DS/P3_huffmancoding.py
@@ -5,63 +5,89 @@ import sys
 2. take out two smallest nodes
 3. combine them to make an internal node.
 '''
+#a min heap is used as a priority queue to pop the 
+#smallest node at the root
 class node_huffman:
+    '''
+    The node class with the left and right child.
+    '''
     def __init__(self,letter):
         self.letter = letter   
-        self.weight = 0
-        self.left_c = None
-        self.right_c = None
+        self.weight = 1
+        self.left = None
+        self.right = None
     #def dummy():
-        # a test to see how this class is imported
+       # a test to see how this class is imported
        
        # print("hello, test")
     def count(self,data):
+        pass
+        # this function counts how many times the letter
+        # appeared in the list. Assign the frequency to the 
+        # weight variable and return the frequency.
         counter = data.count(self.letter)
         self.weight = counter
         return counter
 
 class huffman_tree:
+    # note to self: Does it make sense to build this with a list?
     def __init__(self):
         self.root = None
     
-    def insert(node):
-        pass
+    def insert():
+        # the edges are represented as the children
+        
     def remove():
-        pass
-    def priority():
-        pass
-        #sort the nodes according to frequency
+        
+        if self.root.left >= self.root.right:
+            new_node_freq = self.root.frequency + self.root.right.frequency
+        # how do you compare and reorganize the hierarchy effectively?
+        new_root = self.root.left
+        self.root = self.root.left
+        self.root.left = new_root.left
+        self.root.right = new_root.right
+        else:
+        new_root = self.root.right
+        self.root = self.root.right
+        self.root.left = new_root.left
+        self.root.right = new_root.right
     def heapify():
-        pass
+        if self.root >= 
+        
+        
 def huffman_encoding(data):
     #input: a string
     #output: encoded data and its tree
     
     # convert the string into a list
-    createnode(data)
+    list_node = createnode(data)
 
     
     tree = huffman_tree()
     # sort data and make it into a tree
+    tree.append(createnode(data))
     
 def createnode(data):
     #input: given data, usually a sentence
     #output: node objects used to build a tree
     data = list(data)
-    letterlist = list()
+    letterlist = []
     allnodes = list()
     for i in data:
-        if i and i not in letterlist:
-           
-           letterlist.append(str(i))
-           i = node_huffman(str(i))
-           allnodes.append(i)
-    return allnodes, letterlist
+        if i not in letterlist:
+            letterlist.append(i)
+            i_node = node_huffman(str(i))
+            allnodes.append(i_node)
+        else:
+            #i.frequency += 1
+            #allnodes[i]+=1
+            
+    return allnodes
 def huffman_decoding(data,tree):
     #output: decoded data
     pass
 data = 'I am a big giant tree'
-print(createnode(data))
+#print(createnode(data))
 '''
 if __name__ == "__main__":
     codes = {}
