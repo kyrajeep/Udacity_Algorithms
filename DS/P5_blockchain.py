c263107752e0d4f3fb433498bcdbf5e7e363af84
kyrajeep <jee_won_park@brown.edu>
2020-09-05 16:31:02 -0400

    feat: the class for blocks in the blockchain completed

---
+++ DS/P5_blockchain.py
@@ -0,0 +1,26 @@
+# Each of these blocks become a node in a linked list.
+# SHA256, Greenwich Mean Time (GMT)
+import hashlib
+
+class Block:
+    # Data: text strings
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.next = None
+      self.previous_hash = previous_hash #for linked list
+      self.hash = self.calc_hash()
+    def calc_hash(self):
+        # output: hash value
+        sha = hashlib.sha256()
+
+        hash_str = "We are going to encode this string of data!".encode('utf-8')
+        sha.update(hash_str)
+
+        return sha.hexdigest()
+
+# what would be the corner cases?    
+prev_hash = '012938'
+block1 = Block('13:27 060620', 'drone', prev_hash)
+block2 = Block('12:59 060720', 'vaccuum', prev_hash)
+block1.next = block2
