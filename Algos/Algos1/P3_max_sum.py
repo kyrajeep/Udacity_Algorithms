889d4d8048ccf470998abd5d9e212680169a9e68
kyrajeep <jee_won_park@brown.edu>
2020-09-27 08:11:55 +0900

    test: Add one test case

---
+++ Algos/Algos1/P3_max_sum.py
@@ -0,0 +1,9 @@
+def rearrange(input_array):
+    # input: an array of integers
+    # output: two numbers such that their sum is the max
+    
+
+# test 1
+a1 = [1,4,5]
+# expected output
+#[54, 1]? [51, 4]?
