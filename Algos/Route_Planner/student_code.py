a3892016130e0ddb5e63a2f465023bf72d3b2acd
kyrajeep <jee_won_park@brown.edu>
2020-10-07 13:47:43 +0900

    feat: check adjacent nodes

---
+++ Algos/Route_Planner/student_code.py
@@ -0,0 +1,21 @@
+def shortest_path(M,start,goal):
+    # Inputs: M - the given graph, which is a map.
+    print("shortest path called")
+    return
+
+def adjacency(M, start, goal):
+    for adjacent_list in map.roads:
+        if start in adjacent_list:
+            for node in adjacent_list:
+                score = get_score()
+        
+    # compare the scores somewhere
+    
+def get_score(M, node):
+    # use M.intersections to calculate the score.
+    pass
+def heuristic():
+    pass
+
+    
+simple_test = []
